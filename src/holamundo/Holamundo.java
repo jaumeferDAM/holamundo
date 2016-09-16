443598be41b243377916a90e464198599bde9453
ALUMNEDAM <ALUMNEDAM@192.168.120.145>
2016-09-16 19:54:06 +0200

    aubiendo cambios sobre holamundo

--- src/holamundo/Holamundo.java
+++ src/holamundo/Holamundo.java
@@ -14,7 +14,7 @@ public class Holamundo {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) {    
         System.out.println("Primer proyecto");
     }
     
