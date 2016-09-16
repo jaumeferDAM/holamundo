d9c21d49ade3b47b7ce032827d6a5fa02cb61dbf
jaumeferDAM <jaumefernandezacanmas@gmail.com>
2016-09-16 20:09:01 +0200

    Update Holamundo.java

--- src/holamundo/Holamundo.java
+++ src/holamundo/Holamundo.java
@@ -15,7 +15,7 @@ public class Holamundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {    
-        System.out.println("accacacacacaPrimer proyecto");
+        System.out.println("accSsAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAacaPrimer proyecto");
     }
     
 }
